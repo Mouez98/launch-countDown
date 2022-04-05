5ba0e37010c562c71142bb59849b993af1569bf9
Mouez98 <mouez.matoui@gmail.com>
2022-04-05 23:04:52 +0100

    finished

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
-//Count down 
-const setCountDown = new Date("1 may, 2022, 23:59:59").getTime()
+const domDays = document.getElementById("days");
+const domHours = document.getElementById("hours");
+const domMinutes = document.getElementById("minutes");
+const domSeconds = document.getElementById("seconds");
 
+//Count down
+const setCountDown = new Date().getTime() + 2160000000;
+
+function animation(time, domElement, timeType) {
+  domElement.textContent = `${
+    time < 10 && time >= 0 ? `0${time}` : time < 0 ? "00" : time
+  }`;
+}
 
 const counter = setInterval(() => {
   //Current time
   const curTime = new Date().getTime();
 
-  //Getting time differnce btezeen current time and count down
+  //Getting time differnce bteween current time and count down
   const timeDiff = setCountDown - curTime;
-  
 
   const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const hours = Math.floor(
+    (timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
   const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
 
   //Change the DOM
-  document.getElementById("days").textContent = `${(days < 10 && days >= 0) ? `0 ${days}`: days < 0 ? '00' : days }`;
-  document.getElementById("hours").textContent = `${hours < 10 && hours >= 0 ? `0 ${hours}`: hours < 0 ? '00' : hours }`;
-  document.getElementById("minutes").textContent = `${minutes < 10 && minutes >= 0 ? `0 ${minutes}`: minutes < 0 ? '00' : minutes }`;
-  document.getElementById("seconds").textContent =`${seconds < 10 && seconds >= 0 ? `0 ${seconds}`: seconds < 0 ? '00' : seconds }`;
-  if(timeDiff < 0) {
-      clearInterval(counter)
+  animation(days, domDays);
+  animation(hours, domHours);
+  animation(minutes, domMinutes);
+  animation(seconds, domSeconds, 'seconds');
+  if (timeDiff < 0) {
+    clearInterval(counter);
   }
 }, 1000);
